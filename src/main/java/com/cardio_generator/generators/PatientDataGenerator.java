f7db2bb946c8dea00e1a8986512fea0ae5a2e00b
RemiHeijmans <heijmansremi@gmail.com>
2024-04-20 23:07:43 +0200

    javadoc PatientDataGenerator.java

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,18 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * The PatientDataGenerator interface defines the contract for classes that generate
+ * patient data for all of the health monitoring simulations.
+ * all the classes that generate data use this interface
+ */
 public interface PatientDataGenerator {
+
+    /**
+     * Generates patient data for the specified patient ID and outputs it using the given output strategy.
+     *
+     * @param patientId The ID of the patient for which to generate data  for.
+     * @param outputStrategy The output strategy to use for displaing or storing the generated data.
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
