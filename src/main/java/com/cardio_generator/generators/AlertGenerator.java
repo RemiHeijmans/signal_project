7127149f3ae7bace3295152cca21ed766f9be2e1
RemiHeijmans <heijmansremi@gmail.com>
2024-04-20 23:26:08 +0200

    AlertGenerator.java

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -4,17 +4,30 @@ import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * The AlertGenerator class implements the PatientDataGenerator interface  like said in the interface
+ * to generate simulated alert data for health monitoring simulations.
+ */
 public class AlertGenerator implements PatientDataGenerator {
 
-    //changed variable name camelCase.
     public static final Random randomGenerator = new Random();
     private boolean[] alertStates; // false = resolved, true = pressed
 
-    //corrected constructor name to the class name.
+    /**
+     * Constructs a new AlertGenerator with the given number of patients.
+     *
+     * @param patientCount The number of patients for which to generate alert data.
+     */
     public AlertGenerator(int patientCount) {
         alertStates = new boolean[patientCount + 1];
     }
 
+    /**
+     * Generates alert data for the specified patient ID and outputs it using the output strategy.
+     *
+     * @param patientId The ID of the patient for which to generate data.
+     * @param outputStrategy The output strategy to use for displaying or storing the generated data.
+     */
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
